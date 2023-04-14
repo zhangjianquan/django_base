b989db94e035b6639b144e5f83ec4af3d8e865e9
zhangjianquan <zjqleanr@163.com>
2023-04-14 17:01:22 +0800

    admin站点

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -11,8 +11,11 @@ from django.db import models
 
 class BookInfo(models.Model):
     name=models.CharField(max_length=10)
+    #重写str方法以让admin来显示书籍名字
+    def __str__(self):
+        return self.name
 
 class PeopleInfo(models.Model):
     name = models.CharField(max_length=20)
-    gender = models.BoolenField()
+    gender = models.BooleanField()
     book = models.ForeignKey(BookInfo,on_delete=models.CASCADE)
